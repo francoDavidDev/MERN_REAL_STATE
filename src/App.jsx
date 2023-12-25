10e52b6bc1e988f8e938c9e81cae0a660a33bc29
franco66 <david_franco@outlook.com>
2023-12-25 11:49:52 -0300

    agrege arthivos

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/Signin'
+import SignUp from './pages/SignUp'
+import About from './pages/about'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+          <Route path='/'  element={<Home/>} />
+          <Route path='/sign-in'  element={<SignIn/>} />
+          <Route path='/sign-up'  element={<SignUp/>} />
+          <Route path='/about'  element={<About/>} />
+          <Route path='/profile'  element={<Profile/>} />
+
+      </Routes>
+    </BrowserRouter>
+  
+  )
+}
+
+export default App
